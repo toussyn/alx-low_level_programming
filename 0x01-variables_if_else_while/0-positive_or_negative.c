64a40837981e3a4a6e037eb13180323614acb985
Adijat Ojutomori <aojutomori@gmail.com>
2022-04-08 08:35:52 -0700

    variable directory

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,14 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <time.h> 
-/** 
- * * main - Check if a number is greater than 0
- * * @n: The number to be checked
- * * 
- * * Return: 1 if the number is positive. 0 otherwise
- * */
-int main(void) 
-{    
+/**
+* main - Check if a number is greater than 0
+* @n: The number to be checked
+*
+* Return: 1 if the number is positive. 0 otherwise
+*/
+int main(void)
+{
 	int n;
 
 	srand(time(0));    
