3338da14ef99166d90fd9604dc514a294cc5448a
Adijat Ojutomori <aojutomori@gmail.com>
2022-04-09 01:48:46 -0700

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,26 +10,26 @@ int main(void)
 {
 int i, j, k;
 
-	for (i = 48; i <= 57; i++)
-	{
-		for (j = 49; j <= 57; j++)
+    for (i = 48; i <= 57; i++)
+    {
+        for (j = 49; j <= 57; j++)
+        {
+            for (k = 50; k <= 57; k++)
+            {
+		if (k > j && j > i)
 		{
-			for (k = 50; k <= 57; k++)
-			{
-				if (k > j && j > i)
-				{
-						putchar(i);
-						putchar(j);
-						putchar(k);
-						if (i !=  55 || j != 56)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-				}
-			}
+		    putchar(i);
+		    putchar(j);
+		    putchar(k);
+		    if (i !=  55 || j != 56)
+		    {
+			putchar(',');
+			putchar(' ');
+		    }
 		}
+	    }
 	}
-	putchar('\n');
-	return 0;
+    }
+    putchar('\n');
+    return 0;
 }
