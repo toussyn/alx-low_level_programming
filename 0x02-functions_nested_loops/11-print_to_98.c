ec1b0b09dde8308aae219d4ffe005b97e0a18368
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2022-04-12 19:28:00 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,31 @@
-nxjkzmx
+#include <stdio.h>
+
+/**
+*print_to_98 -  all natural numbers from n to 98
+*@n: an integer
+*Return: void
+void print_to_98(int n)
+{
+	 if (n<=98)
+	 {
+	 	for (i=n; i<=98; i++)
+		{
+			printf("%d", i);
+			if (i!=98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else if (n>98)
+	{
+		for (i=n; i>=98; i--)
+		{
+			printf("%d", i);
+			if (i!=98)
+			{
+				printf(", ");
+			}
+		}
+	}
+}
