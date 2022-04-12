308f99e3a35cb32e36b605c2c312d7d6c385a566
Adijat Ojutomori <aojutomori@gmail.com>
2022-04-12 05:59:32 -0700

    file 3 created

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_islower - checks for lowercase character
+*
+*
+*Return: 1 if lowercase
+*/
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
