7210a7f3b06c324e009bdd82f4cb19934b60c3aa
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2022-04-14 12:06:47 +0100

    Create 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main,h"
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c: the intege to be checked
+ * Returns 1 if c is uppercase, 0 otherwise
+ */
+ 
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
