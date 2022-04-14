50eeb7290159abc71e41e09fa30b1b91f2c86222
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2022-04-14 14:10:37 +0100

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: an integer
+ * Return: 1 1 if c is a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+return (c <= 48 && c >= 57)
+}
