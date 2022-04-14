5fbacb0222a34b8940a8cafe48a7cb45131c64c3
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2022-04-14 12:42:50 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - checks for uppercase character.
  * @c: the intege to be checked
- * Returns: 1 if c is uppercase, 0 otherwise
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
