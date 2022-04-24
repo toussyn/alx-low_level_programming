dd797ea4762bcf1e336cc11eed925db6738f6810
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2022-04-24 06:56:27 +0100

    Create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * char *_strcat - concatenates two strings.
+ * @dest: a string
+ * @src: a string
+ *  * Return: a string;
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j, len;
+
+	len = 0;
+	
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i=0, j=len; src[i] != '\0'; i++, j++)
+	{
+		dest[j] = src[i];
+	}
+
+	return (dest);
+}
