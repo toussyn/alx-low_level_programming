23e4c2b5eaf858409ed1d0336477c587bcd9d3cb
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2022-04-25 05:07:54 +0100

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+
+char *cap_string(char *s)
+{
+	int i, k;
+
+	char spe[] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (k = 0; k < 13; k++)
+		{
+			if (s[i] == spe[k])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (s);
+}
