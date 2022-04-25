7a10ec2345eee7ee3ee759e611eabc72804a57cf
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2022-04-25 04:45:49 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,15 +8,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	i = 0;
-
-	while (*(s1 + i) == *(s2 + i))
+	while (*s1 == *s2)
 	{
-		if (*(s + i) == '\0')
-			break;
-		i++;
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
 	}
-	return (*(s1 + i) - *(s2 + i))
+	return (*s1 - *s2);
 }
